cd1922fc6cbc6b99e64f3311afc0cded1e381898
Mario Peñas Moreno <jeroboan2002@yahoo.es>
2022-07-12 06:22:33 +0200

    Genero el componente formulario

---
+++ src/app/components/formulario-component/formulario-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-component',
+  templateUrl: './formulario-component.component.html',
+  styleUrls: ['./formulario-component.component.css']
+})
+export class FormularioComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
