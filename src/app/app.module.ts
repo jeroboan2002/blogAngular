ad69255b046bbd1bcfd17252afa1bb4dab8b65cf
Mario Peñas Moreno <jeroboan2002@yahoo.es>
2022-07-16 05:08:01 +0200

    Añado librería ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponentComponent } from './components/blog-component/blog-component.component';
 import { FormularioComponentComponent } from './components/formulario-component/formulario-component.component';
 import { ViewPostComponent } from './components/view-post/view-post.component';
+
 
 
 
@@ -22,6 +24,9 @@ import { ViewPostComponent } from './components/view-post/view-post.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
