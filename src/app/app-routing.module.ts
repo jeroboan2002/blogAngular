f6db0a7cdfa4f40765e7f26fe3170cd970a23b89
Mario Peñas Moreno <jeroboan2002@yahoo.es>
2022-07-16 05:02:36 +0200

    creación de ruta hija

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,13 @@ import { ViewPostComponent } from './components/view-post/view-post.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: "/home"},
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, children: [
+    {path: '', component: HomeComponent},
+    {path: 'category/:categoryTitle', component: HomeComponent},
+  ]
+  },
   {path: 'new', component: FormularioComponentComponent},
   {path: 'post/:idpost', component: ViewPostComponent},
-  {path: 'category/:categoryTitle', component: HomeComponent},
   {path: "**", redirectTo: 'home'}
 ];
 
