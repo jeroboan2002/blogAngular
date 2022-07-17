e82c6767675836ea84785ca8336a8cd9f6a9c8a6
Mario Peñas Moreno <jeroboan2002@yahoo.es>
2022-07-17 17:13:20 +0200

    Verifico los validadores

--- src/app/components/formulario-component/formulario-component.component.ts
+++ src/app/components/formulario-component/formulario-component.component.ts
@@ -13,6 +13,7 @@ export class FormularioComponentComponent implements OnInit {
 
   formulario: FormGroup
   arrCategorias: Category[] = []
+  
 
   constructor(
     private categoriesService: CategoriesService,
@@ -28,17 +29,18 @@ export class FormularioComponentComponent implements OnInit {
           Validators.pattern(/^(https):\/\/[^ "]+$/)
         ]),
         date: new FormControl('', [
-          Validators.required
+          Validators.required,
         ]),
         category: new FormControl('', [
-          Validators.required
+          Validators.required,
         ]),
         title: new FormControl('', [
           Validators.required,
           Validators.minLength(3)
         ]),
         text: new FormControl('', [
-          Validators.required
+          Validators.required,
+          Validators.minLength(100)
         ])
       })
     }
@@ -47,19 +49,21 @@ export class FormularioComponentComponent implements OnInit {
     this.arrCategorias = this.categoriesService.getAllCategories()
   }
 
-  getDataForm(formulario: any){
-    console.log(formulario.value)
-    this.postService.agregarPost(this.formulario.value)
-    this.formulario.reset({})
-    alert('Tu Post ha sido correctamente creado')
-  }
-
   checkControl(controlName: string, errorName: string): boolean{
     if(this.formulario.get(controlName)?.hasError(errorName) && this.formulario.get(controlName)?.touched){
       return true;
     }else{
       return false;
     }
+  }
+
+
+
+  getDataForm(formulario: any){
+    this.postService.agregarPost(this.formulario.value)
+    this.formulario.reset({})
+    alert('Tu Post ha sido correctamente creado, vuelve a Inicio para verlo')
+    
   }
 
   
